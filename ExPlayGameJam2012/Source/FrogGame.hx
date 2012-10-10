83810937509ab4b89fafb1aa9f079623872369ea
James Frost <james@jameswfrost.co.uk>
2012-10-10 22:21:14 +0100

    Missed a change from the previous commit.

--- ExPlayGameJam2012/Source/FrogGame.hx
+++ ExPlayGameJam2012/Source/FrogGame.hx
@@ -6,6 +6,8 @@ class FrogGame extends FlxGame
 {
 	public function new()
 	{
+		Global.roundNumber = 1;
+
 		super(1024, 560, MenuState, 1); //Create a new FlxGame object at 800x480 with 1x pixels, then load PlayState
 	
 		#if !neko
